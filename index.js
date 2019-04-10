cf2a3117f36fed4c02d4c57a99b564ef87fb6538
sutaking <zhaofeng.vip@gmail.com>
2019-04-10 16:07:33 +0800

    launch done

--- index.js
+++ index.js
@@ -20,12 +20,15 @@ let test = new Sdb({
     tv:"109.123.121.93",
     user: "root",
     pwd: "tizen",
-    host: '109.123.121.146'
+    host: '109.123.121.146',
+    appUUid: '3201701011486',
+    appTizenId: 'zcwsruDJBm.CAPHTESTSUITEANGULAR'
 });
 
 test.connect();
-test.installByWgt('/home/zhaof/10.webDriver/3201701011486_1.0.2.wgt');
-test.launch();
+
+test.installByFile('/home/zhaof/10.webDriver/3201701011486_1.0.2.wgt', 'b');
+test.launch('b');
 
 
 
--- src/sdb.js
+++ src/sdb.js
@@ -1,13 +1,17 @@
 'use strict';
 const { execSync } = require('child_process');
+const path = require('path');
+
 const { sdbPath } = require('./utils');
 
 class Sdb {
     constructor(config) {
         this.config = config;
+        this.serPort = 26101;
 
         // remote path for webapp install on Tv.
         this.remotePath = '/opt/usr/home/';
+
         //console.log(sdbPath);
     }
 
@@ -21,7 +25,12 @@ class Sdb {
     }
 
     bash(cmd) {
-        console.log(`[Exec Shell]:${cmd}`);
+        if (!cmd) {
+            return console.error('Empty Command! Please check your input');
+        }
+        console.log('Sdb.js Need Install sshpass first.');
+
+        console.log(`[Exec Bash]:${cmd}`);
         const { tv, user, pwd } = this.config;
 
         let execStr = `sshpass -p ${pwd} ssh ${user}@${tv} "${cmd}"`;
@@ -51,9 +60,11 @@ ${result.toString().trim()}
     }
 
     runCli(cmd) {
+        if (!cmd) {
+            return console.error('Empty Command! Please check your input');
+        }
 
-
-        console.log(`runCli: [${sdbPath} ${cmd}]`);
+        console.log(`Exec: [${sdbPath} ${cmd}]`);
 
         this._exec(`${sdbPath} ${cmd}`);
     }
@@ -62,42 +73,54 @@ ${result.toString().trim()}
         this.runCli('kill-server');
         this.runCli('start-server');
     }
+    _switchRoot() {
+        this.runCli(`-s ${this.config.tv} root on`);
+    }
+    sdbShell(cmd) {
+        const { tv } = this.config;
+        this.runCli(`-s ${tv} shell ${cmd}`);
+    }
 
     connect() {
         //this._setDevMode();
         this._restartServer();
 
-        const { tv } = this.config;
-
         this.runCli('disconnect');
-        this.runCli(`connect ${tv}:26101`);
+        this.runCli(`connect ${this.config.tv}:${this.serPort}`);
         this.runCli('devices');
 
-        this.runCli(`-s ${tv} root on`);
+        this._switchRoot();
     }
+    push(file, dst, name) {
+        // Need Del first before
+        this.sdbShell(`rm -rf ${dst}${name}`);
 
-
-    launch() {
-        const { tv } = this.config;
-        this.runCli(`-s ${tv} shell wascmd -r a`);
+        // Push New file to remote
+        this.runCli(`-s ${this.config.tv} push ${file} ${dst}`);
     }
-    kill() {
-
+    launch(appName) {
+        if (!appName) {
+            return console.error('Can not launch App without App ID');
+        }
+        this.sdbShell(`wascmd -r ${appName}`);
     }
-    installByWgt(wgtPath) {
-        const { tv } = this.config;
-        const wgtName = '3201701011486_1.0.2.wgt';
+    kill(appName) {
+        this.sdbShell(`wascmd -r ${appName}`);
+    }
+    uninstall(appName) {
+        this.sdbShell(`wascmd -u ${appName}`);
+    }
+    installByFile(wgtPath, appName) {
+        const wgtName = path.basename(wgtPath);
 
-        this.runCli(`-s ${tv} push ${wgtPath} ${this.remotePath}`);
+        this.push(wgtPath, this.remotePath, wgtName);
 
         const tvPath = `${this.remotePath}${wgtName}`;
-        this.runCli(`-s ${tv} shell wascmd -i a -p ${tvPath}`);
+        this.uninstall(appName);
+        this.sdbShell(`wascmd -i ${appName} -p ${tvPath}`);
     }
-    installByTpk() {
-
-    }
-    installByAppId() {
-
+    installByAppId(appId) {
+        this.sdbShell(`wascmd -i ${appId}`);
     }
     getDebugPort() {
 
